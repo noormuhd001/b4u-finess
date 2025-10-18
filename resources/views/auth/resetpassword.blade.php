967324e3640691b1ef53d8e991186041f7eeab19
Noor muhammed <noormuhammed@bytekat.com>
2025-10-18 10:07:07 +0530

    split the login styles to another file

--- resources/views/auth/resetpassword.blade.php
+++ resources/views/auth/resetpassword.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 
 <body class="d-flex align-items-center justify-content-center vh-100 bg-gradient-primary login-bg">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 
 </head>
 
